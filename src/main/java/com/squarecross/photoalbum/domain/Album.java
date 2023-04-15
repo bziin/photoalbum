cc093da9217d0ea71d7e4dd35c6a1f95b4a32cd6
bzzzzi@naver.com <bzzzzi@naver.com>
2023-04-15 21:26:43 +0900

    repository

--- src/main/java/com/squarecross/photoalbum/domain/Album.java
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -3,6 +3,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name="album", schema = "photo_album",uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
@@ -19,6 +20,9 @@ public class Album {
     @Column(name="created_at", unique = false, nullable= true)
     @CreationTimestamp
     private Date createdAt;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos;
 
     public Album() {};
 
